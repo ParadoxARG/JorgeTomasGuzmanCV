57ecd5e78ae0684136b7c7a8f8909b4e3c902127
Tomas Guzmán <tomyguzman@hotmail.com>
2018-10-11 14:33:37 -0300

    Cambios esteticos y primer intento de cambio de idioma(desactivado por el momento)

--- JorgeTomasGuzmanCV/WebApplication1/wwwroot/js/site.js
+++ JorgeTomasGuzmanCV/WebApplication1/wwwroot/js/site.js
@@ -2,20 +2,59 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$("body").ready(function () {
+    var getUrlParameter = function getUrlParameter(sParam) {
+        var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+            sURLVariables = sPageURL.split('&'),
+            sParameterName,
+            i;
+
+        for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
+
+            if (sParameterName[0] === sParam) {
+                return sParameterName[1] === undefined ? true : sParameterName[1];
+            }
+        }
+    };
+
+    var language = getUrlParameter('language');
+    if (language) {
+        if (language == "es")
+            document.body.className = 'es';
+        if (language == "en")
+            document.body.className = 'en';
+        //alert("change body");
+    }
+
+});
 
 $(document).ready(function () {
     $('.collapsible').collapsible();
     $('.sidenav').sidenav();
     $('.tabs').tabs();
-//    $('.button-collapse').sideNav();
     $('.materialboxed').materialbox();
+
+    
     $('.btn').click(function () {
-        document.body.className = 'es';
+        if ($(this).attr("id") == "es") 
+            document.body.className = 'es';
+        if ($(this).attr("id") == "en") 
+            document.body.className = 'en';
     });
-    $("#es").click(function () {
-        alert('es');
+    //$(".btn#es").on('click', function () {
+    //    alert('1');
+    //});
+    
+    $("a#conocimientos").click( function (e) {
+        e.preventDefault();
+        if (document.body.className === 'es')
+            $('input[name="language"]').val("es");
+        if (document.body.className === 'en')
+            $('input[name="language"]').val("en");
+        document.getElementById('conocimientosform').submit();
     });
-    $("#en").click(function () {
-        alert('en');
-    });
+
+    
+
 });
